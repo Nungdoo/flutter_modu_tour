8020e4f81706a20f4df8060807bd3055b9f78175
nungdoo <hola.coco93@gmail.com>
2023-08-07 12:35:03 +0900

    move user.dart

--- lib/login.dart
+++ lib/login.dart
@@ -5,7 +5,7 @@ import 'dart:math';
 import 'package:crypto/crypto.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:modu_tour/user.dart';
+import 'package:modu_tour/data/user.dart';
 
 class LoginPage extends StatefulWidget {
   @override
